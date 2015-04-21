ef083f45ab5047c4e0d21a3ad88beea213b2bc0e
Jerome Noveda <noveda.jerome@gmail.com>
2015-04-21 22:22:58 +0800

    add the css,js, and fonts files and applied mockup to header and footer

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,5 +1,20 @@
-		<div>
-			Footer
-		</div>
+<!-- footer -->
+
+	<div class="text-center">&copy LNU, Archive Thesis 2014</div>
+
+	<!-- footer end -->
+		
 	</body>
 </html>
+<script type="text/javascript" src="public/js/jquery.js"></script>
+<script type="text/javascript" src="public/js/bootstrap.js"></script>
+<script type="text/javascript" src="public/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="public/js/npm.js"></script>
+
+<script type="text/javascript">
+$(document).ready(function(e){
+	$('#add-user-button').click(function(e){
+		$('#modal-add-user').modal('show');
+	});
+});
+</script>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -2,29 +2,41 @@
 <html>
 	<head>
 		<title>Archive Thesis</title>
+		<!-- Bootstrap stylesheets -->
+	   
+	    <link rel="stylesheet" type="text/css" href="public/css/bootstrap-theme.css">
+	    <link rel="stylesheet" type="text/css" href="public/css/bootstrap-theme.css.map">
+	    <link rel="stylesheet" type="text/css" href="public/css/bootstrap-theme.min.css">
+	    <link rel="stylesheet" type="text/css" href="public/css/bootstrap.css">
+	    <link rel="stylesheet" type="text/css" href="public/css/bootstrap.css.map">
+	    <link rel="stylesheet" type="text/css" href="public/css/bootstrap.min.css">
 	</head>
-
 	<body>
-		<div>
-			Header
-			<ul>
-				<li>
-					<a href="<?php echo base_url(); ?>">Home</a>
+		<!-- header -->
+
+	<div class="navbar navbar-default" role="navigation">
+		<div class="navbar-header">
+			<a href="#" class="navbar-brand">Archive Thesis</a>
+		</div>
+		<div class="collapse navbar-collapse">
+			<ul class='nav nav-tabs navbar-right'>
+				<li role="presentation">
+					<a href="<?php echo base_url(); ?>" role="tab">Home</a>
 				</li>
 				<?php if($user_id != null): ?>
-				<li>
-					<a href="<?php echo base_url(); ?>theses">Theses</a>
+				<li role="presentation">
+					<a href="<?php echo base_url(); ?>theses" role="tab">Theses</a>
 				</li>
-				<li>
-					<a href="<?php echo base_url(); ?>user">Users</a>
+				<li role="presentation">
+					<a href="<?php echo base_url(); ?>user" role="tab">Users</a>
 				</li>
-				<li>
-					<!-- brad ini nga general settings amo ini an mga settings hini nga system sugad han add, edit, delete han category, course, roles ha usa la nga page basta an tanan nga settings -->
-					<a href="<?php echo base_url(); ?>settings">General Settings</a>
+				<li role="presentation">
+					<a href="<?php echo base_url(); ?>settings" role="tab">General Settings</a>
 				</li>
-				<li>
-					<a href="<?php echo base_url(); ?>user/logout">Logout</a>
+				<li role="presentation">
+					<a href="<?php echo base_url(); ?>user/logout" role="tab">Logout</a>
 				</li>
 				<?php endif; ?>
 			</ul>
 		</div>
+	</div>
